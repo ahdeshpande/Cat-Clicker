bc80b88bf524180d174927a51ac48d209e4e426f
ahdeshpande <deshpande.abhiraj@gmail.com>
2018-01-21 09:00:18 -0500

    Increased cat count from 1 to 2

--- js/click-me.js
+++ js/click-me.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
 
+	var cat_names = ['Ashes', 'Smoky']
+
+	$('.cat-name-1').text(cat_names[0])
+	$('.cat-name-2').text(cat_names[1])
+
 	$('.cat-image').on('click', function(){
-		$('.pet-count').text(parseInt($('.pet-count').text()) + 1)
+		if($(this).hasClass('cat-1')){
+			$('.pet-count-1').text(parseInt($('.pet-count-1').text()) + 1)
+		}
+		else {
+			$('.pet-count-2').text(parseInt($('.pet-count-2').text()) + 1)
+		}
 	})
 
 })
