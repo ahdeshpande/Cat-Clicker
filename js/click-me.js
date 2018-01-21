148ef67edb596c48c9244965313b4dc743f8088a
ahdeshpande <deshpande.abhiraj@gmail.com>
2018-01-21 10:35:37 -0500

    Increase the cat count to 5

--- js/click-me.js
+++ js/click-me.js
@@ -1,17 +1,14 @@
 $(document).ready(function(){
 
-	var cat_names = ['Ashes', 'Smoky']
+	var cat_names = ['Ashes', 'Smoky', 'Kitty', 'Oscar', 'Misty']
 
-	$('.cat-name-1').text(cat_names[0])
-	$('.cat-name-2').text(cat_names[1])
-
+	// Add names
+	for(var i=0; i<cat_names.length; i++){
+		$('.cat-name-' + (i+1)).text(cat_names[i])	
+	}
+	
 	$('.cat-image').on('click', function(){
-		if($(this).hasClass('cat-1')){
-			$('.pet-count-1').text(parseInt($('.pet-count-1').text()) + 1)
-		}
-		else {
-			$('.pet-count-2').text(parseInt($('.pet-count-2').text()) + 1)
-		}
+		$('.pet-count-' + $(this).attr('name')).text(parseInt($('.pet-count-' + $(this).attr('name')).text()) + 1)
 	})
 
 })
